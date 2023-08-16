a4cfa3af7a8605bba99957b657b3b282e059eafb
veoquynhs <hieu.tt2304@gmail.com>
2023-08-16 23:27:23 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+1. Upgrade Box -> BoxV2
+2. Proxy -> Box
+         -> BoxV2
+
+1. Deploy a Proxy manually
+2. hardhat-deploy's built-in proxies
+3. Openzeppelin upgrades plugin
---
+++ helper-hardhat-config.js
@@ -0,0 +1,30 @@
+const networkConfig = {
+    default: {
+        name: "hardhat"
+    },
+    31337: {
+        name: "localhost",
+        subscriptionId: "588",
+        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
+        callbackGasLimit: "500000", // 500,000 gas
+    },
+    11155111: {
+        name: "sepolia",
+        subscriptionId: "588",
+        gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c", // 30 gwei
+        callbackGasLimit: "500000", // 500,000 gas
+    },
+    1: {
+        name: "mainnet",
+        keepersUpdateInterval: "30",
+    },
+}
+
+const developmentChains = ["hardhat", "localhost"]
+const VERIFICATION_BLOCK_CONFIRMATIONS = 6
+
+module.exports = {
+    networkConfig,
+    developmentChains,
+    VERIFICATION_BLOCK_CONFIRMATIONS,
+}
